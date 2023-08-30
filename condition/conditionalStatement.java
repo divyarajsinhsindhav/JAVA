00d17c0963a154418f66d06133a63e6e82bb796a
Divyrajsinh Sindhav <divyrajsinh@Divyrajsinhs-MacBook-Air.local>
2023-08-30 23:45:49 +0530

    30th Aug 2023

---
+++ condition/conditionalStatement.java
@@ -0,0 +1,23 @@
+package condition;
+import java.util.*;
+public class conditionalStatement{
+    public static void main(String[] args){
+        //Largest from 3
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        int a = scn.nextInt();
+        System.out.println("Enter number 2: ");
+        int b = scn.nextInt();
+        System.out.println("Enter number 3: ");
+        int c = scn.nextInt();
+        if(a>b && a>c){
+            System.out.println("Number a is Largest");
+        }
+        else if(b>a && b>c){
+            System.out.println("Number b is Largest");
+        }
+        else{
+            System.out.println("Number c is Largest");
+        }
+    }
+}
