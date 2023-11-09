f7aa2fc335977b8c18d1eb9e91d5911afada5117
Divyrajsinh Sindhav <divyrajsinh3315@gmail.com>
2023-11-09 16:25:34 +0530

    Inheritance

---
+++ OOPS/Inheritance.java
@@ -0,0 +1,50 @@
+package OOPS;
+
+/**
+ * Inheritance
+ */
+public class Inheritance {
+
+    public static void main(String[] args) {
+        Fish Shark = new Fish();
+        // Shark.eat();
+
+        Dog tobby= new Dog();
+        tobby.eat();
+        tobby.legs = 4;
+        System.out.println(tobby.legs);
+    }
+}
+
+//Base Class
+//Parant
+class Animal{
+    String color;
+    
+    void eat(){
+        System.out.println("Eats");
+    }
+    void breath(){
+        System.out.println("Breath");
+    }
+}
+
+//Derived Class
+//Child
+//This is Example of Single Level Inheritance
+class Fish extends Animal{
+    int fins;
+
+    void swims(){
+        System.out.println("Swim in the Water");
+    }
+}
+
+class Mammal extends Animal{
+    int legs;
+}
+
+//This is Example of Multilevel Inheritance
+class Dog extends Mammal{
+    String breed;
+}
